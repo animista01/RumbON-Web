4b4df8a36a662ba17f6baa095227bdd7c3d799af
Bryan Villafane <briananime07@gmail.com>
2013-06-04 21:53:43 -0500

    Readme

--- application/views/user/profile.blade.php
+++
@@ -1,5 +0,0 @@
-@layout('master')
-@section('content')
-	<h2>{{ HTML::entities(Auth::user()->name) }}</h2>
-	<img src="{{ URL::base(); }}/uploads/thumbnails/users/{{ Auth::user()->image }}" alt="{{ Auth::user()->name }}" />
-@endsection
